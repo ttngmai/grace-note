450caec24081e867c548c88b22bf6aa31c31e60f
ttngmai <63570797+ttngmai@users.noreply.github.com>
2026-09-18 23:39:26 +0900

    feat(bible): 원어사전 목록에 원어설명2 추가 (#9)

--- src/shared/constants.ts
+++ src/shared/constants.ts
@@ -32,7 +32,7 @@ export const PANEL_CATEGORIES_AND_VERSIONS: { [key in PanelCategory]: string[] }
   ],
   [PanelCategory.COMMENTARY]: ['매튜헨리', '성경관주', '풀핏주석'],
   [PanelCategory.CODED_BIBLE]: ['개역개정S', '개역한글S'],
-  [PanelCategory.LEXICON]: ['원어설명', '원어분해'],
+  [PanelCategory.LEXICON]: ['원어설명', '원어설명2', '원어분해'],
   [PanelCategory.NONE]: []
 }
 
